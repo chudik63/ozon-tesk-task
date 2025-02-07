36f88eaf8f60d2f2600020b1fcea886336dbf285
Vitalya <vitamin11182005@gmail.com>
2025-02-07 15:28:28 +0300

    add no change error handling

--- internal/database/postgres/migrate.go
+++ internal/database/postgres/migrate.go
@@ -1,14 +1,18 @@
 package postgres
 
 import (
+	"context"
 	"fmt"
+	"ozon-tesk-task/pkg/logger"
 
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
-func Migrate(cfg Config, source string) error {
+func Migrate(ctx context.Context, cfg Config, source string) error {
+	l := logger.GetLoggerFromCtx(ctx)
+
 	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", cfg.UserName, cfg.Password, cfg.Host, cfg.Port, cfg.DbName)
 
 	m, err := migrate.New("file://"+source, dbURL)
@@ -16,9 +20,17 @@ func Migrate(cfg Config, source string) error {
 		return fmt.Errorf("failed to create migration: %w", err)
 	}
 
-	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+	err = m.Up()
+	if err == migrate.ErrNoChange {
+		l.Info(ctx, "no change after migration")
+		return nil
+	}
+
+	if err != nil {
 		return fmt.Errorf("failed to make migration up: %w", err)
 	}
+
+	l.Info(ctx, "migration complete succesfully")
 
 	return nil
 }
