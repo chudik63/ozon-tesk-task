34485d86f41320f208676fefb4cd9e925b9d82b9
Vitalya <vitamin11182005@gmail.com>
2025-02-07 11:22:57 +0300

    add logger

---
+++ pkg/logger/logger.go
@@ -0,0 +1,105 @@
+package logger
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	LoggerKey   = "logger"
+	RequestID   = "requestID"
+	ServiceName = "service"
+)
+
+type Logger interface {
+	Debug(ctx context.Context, msg string, fields ...zap.Field)
+	Info(ctx context.Context, msg string, fields ...zap.Field)
+	Error(ctx context.Context, msg string, fields ...zap.Field)
+	Fatal(ctx context.Context, msg string, fields ...zap.Field)
+}
+
+type logger struct {
+	serviceName string
+	logger      *zap.Logger
+}
+
+func (l logger) Debug(ctx context.Context, msg string, fields ...zap.Field) {
+	fields = append(fields, zap.String(ServiceName, l.serviceName))
+
+	if ctx.Value(RequestID) != nil {
+		fields = append(fields, zap.String(RequestID, ctx.Value(RequestID).(string)))
+	}
+	l.logger.Debug(msg, fields...)
+}
+
+func (l logger) Info(ctx context.Context, msg string, fields ...zap.Field) {
+	fields = append(fields, zap.String(ServiceName, l.serviceName))
+
+	if ctx.Value(RequestID) != nil {
+		fields = append(fields, zap.String(RequestID, ctx.Value(RequestID).(string)))
+	}
+	l.logger.Info(msg, fields...)
+}
+
+func (l logger) Error(ctx context.Context, msg string, fields ...zap.Field) {
+	fields = append(fields, zap.String(ServiceName, l.serviceName))
+
+	if ctx.Value(RequestID) != nil {
+		fields = append(fields, zap.String(RequestID, ctx.Value(RequestID).(string)))
+	}
+	l.logger.Error(msg, fields...)
+}
+
+func (l logger) Fatal(ctx context.Context, msg string, fields ...zap.Field) {
+	fields = append(fields, zap.String(ServiceName, l.serviceName))
+
+	if ctx.Value(RequestID) != nil {
+		fields = append(fields, zap.String(RequestID, ctx.Value(RequestID).(string)))
+	}
+	l.logger.Fatal(msg, fields...)
+}
+
+func New(serviceName string) (Logger, error) {
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "timestamp",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "message",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.CapitalColorLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+
+	config := zap.Config{
+		Level:            zap.NewAtomicLevelAt(zap.DebugLevel),
+		Development:      true,
+		Encoding:         "console",
+		EncoderConfig:    encoderConfig,
+		OutputPaths:      []string{"stdout"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+
+	zapLogger, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return &logger{
+		serviceName: serviceName,
+		logger:      zapLogger,
+	}, nil
+}
+
+func (l logger) Stop() {
+	_ = l.logger.Sync()
+}
+
+func GetLoggerFromCtx(ctx context.Context) Logger {
+	return ctx.Value(LoggerKey).(Logger)
+}
