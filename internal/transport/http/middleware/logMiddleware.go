67fab0ea2073e6e247da49ccf89cb1e6b7cfebb9
Vitalya <vitamin11182005@gmail.com>
2025-02-10 18:51:02 +0300

    add request id in logging

--- internal/transport/http/middleware/logMiddleware.go
+++ internal/transport/http/middleware/logMiddleware.go
@@ -5,14 +5,27 @@ import (
 	"ozon-tesk-task/pkg/logger"
 
 	"github.com/99designs/gqlgen/graphql"
+	"github.com/google/uuid"
 	"go.uber.org/zap"
 )
 
-func LogMiddleware(logger logger.Logger) graphql.OperationMiddleware {
+const requestIDHeader = "X-Request-ID"
+
+func LogMiddleware(log logger.Logger) graphql.OperationMiddleware {
 	return func(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
 		opCtx := graphql.GetOperationContext(ctx)
 
-		logger.Debug(ctx, "request", zap.String("operation name", opCtx.OperationName))
+		req := opCtx.Headers.Get(requestIDHeader)
+		if req == "" {
+			newUUID, err := uuid.NewUUID()
+			if err == nil {
+				req = newUUID.String()
+			}
+		}
+
+		ctx = context.WithValue(ctx, logger.RequestID, req)
+
+		log.Debug(ctx, "request", zap.String("operation name", opCtx.OperationName))
 
 		return next(ctx)
 	}
