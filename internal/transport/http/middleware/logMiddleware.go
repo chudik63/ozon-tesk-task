fa25172e58eb36bdd39df29aef5740037b91d5d7
Vitalya <vitamin11182005@gmail.com>
2025-02-10 03:35:17 +0300

    edit middleware

--- internal/transport/http/middleware/logMiddleware.go
+++ internal/transport/http/middleware/logMiddleware.go
@@ -3,7 +3,6 @@ package middleware
 import (
 	"context"
 	"ozon-tesk-task/pkg/logger"
-	"time"
 
 	"github.com/99designs/gqlgen/graphql"
 	"go.uber.org/zap"
@@ -13,16 +12,8 @@ func LogMiddleware(logger logger.Logger) graphql.OperationMiddleware {
 	return func(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
 		opCtx := graphql.GetOperationContext(ctx)
 
-		start := time.Now()
-
 		logger.Debug(ctx, "request started", zap.String("operation name", opCtx.OperationName))
 
-		responseHandeler := next(ctx)
-
-		dur := time.Since(start)
-
-		logger.Debug(ctx, "request finished", zap.String("operation name", opCtx.OperationName), zap.Any("duration", dur))
-
-		return responseHandeler
+		return next(ctx)
 	}
 }
